89eabd32c9664fd0fc4e6fd3c19b9e4eed5de5ee
Jaewon <toa7506@gmail.com>
2022-11-15 17:26:17 +0900

    feat: test-request API

--- app/src/main/java/com/mask/api/domain/problem/dao/ProblemRepository.java
+++ app/src/main/java/com/mask/api/domain/problem/dao/ProblemRepository.java
@@ -4,5 +4,9 @@ import com.mask.api.domain.problem.domain.Problem;
 import com.mask.api.domain.user.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ProblemRepository extends MongoRepository<Problem,String> {
+    List<Problem> findProblemsByLevel(String level);
+    Problem findProblemsByLevelAndIdx(String level,Integer idx);
 }
