ce0b67545948b89b756cf8c7ec95b2ad6995e145
Jaewon <toa7506@gmail.com>
2022-11-15 20:41:46 +0900

    refactor: dto 관련 코드 이동

--- app/src/main/java/com/mask/api/domain/user/dto/LoginRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.mask.api.domain.user.dto;
-
-import lombok.Getter;
-
-@Getter
-public class LoginRequestDto {
-    private String email;
-}
-
--- app/src/main/java/com/mask/api/domain/user/dto/LogoutRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.mask.api.domain.user.dto;
-
-import lombok.Getter;
-
-@Getter
-public class LogoutRequestDto {
-    private String email;
-    private String token;
-}
